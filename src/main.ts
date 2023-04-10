5b74d669922110f57e6fcff89a253249fc316b7f
Liu Hongjian <liuhongjian0316@outlook.com>
2023-04-10 15:34:42 +0800

    练习案例

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import 'vue-cesium/dist/index.css'
 import {i18n} from '@/i18n/index'
 import {pinia} from '@/store/index'
 import register from "@/components/index";
-
+(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
 const app = createApp(App)
 app.use(router)
     .use(i18n)
